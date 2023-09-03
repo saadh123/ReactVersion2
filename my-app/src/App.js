f03a06df66607558740f69460c723643baa89675
Saad H <xcv27514@gmail.com>
2023-09-02 20:28:34 -0400

    React Router Dom added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.css";
 import Images from "./components/Images";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-function App() {
-  // const [isShowing, setisShowing] = useState(false);
-
-  // function handleClick() {
-  //   setisShowing(!isShowing);
-  // }
-
+function Gallery() {
   return (
     <section className="flex justify-center">
       <div className="w-10/12">
@@ -16,17 +11,55 @@ function App() {
           <Images />
         </div>
         {/* <button
-          className="p-1 bg-blue-700 text-white my-2"
-          onClick={handleClick}
-        >
-          Toggle Image
-        </button>
+      className="p-1 bg-blue-700 text-white my-2"
+      onClick={handleClick}
+    >
+      Toggle Image
+    </button>
 
-        {isShowing ? <Images /> : null} */}
+    {isShowing ? <Images /> : null} */}
       </div>
     </section>
   );
 }
+
+function Home() {
+  return (
+    <div className="flex h-screen">
+      <h1 className="m-auto text-3xl">Welcome Home</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div className="flex h-screen">
+      <h1 className="m-auto text-3xl">Login Page</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Routes>
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </Router>
+  );
+}
+
+// const [isShowing, setisShowing] = useState(false);
+
+// function handleClick() {
+//   setisShowing(!isShowing);
+// }
 
 // // function App() {
 // //   const [title, setTitle] = useState("Hello React");
